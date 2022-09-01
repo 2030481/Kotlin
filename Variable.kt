0f984c7168df3482fa6391735fb75e5871f6d8f1
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-01 17:15:28 +0530

    Create Variable.kt

---
+++ Variable.kt
@@ -0,0 +1,20 @@
+fun main(){
+var wholeNumber=18
+  var fractionalNumber=3.5
+  var sentence ="Hey this is my First Line Code"
+  var condition = false
+ 
+  val number = 23545
+  val decimal = 5.67
+  val name ="Ravi Ranjan"
+  val conditions = true
+  
+  println(wholeNumber)
+  println(fractionalNumber)
+  println(sentence)
+    println(condition)
+  println(number)
+  println(decimal) 
+  println(name)
+  println(conditions)
+}
