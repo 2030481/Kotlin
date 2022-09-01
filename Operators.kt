545ada317f56a2f4175a995f40f8838abf3581d5
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-01 23:40:46 +0530

    Create Operators.kt

---
+++ Operators.kt
@@ -0,0 +1,23 @@
+/**
+ * Varoius Operators in Kotlin
+ * Addition
+ * Subtraction
+ * Multiplication and other operators
+ */
+fun main() {
+val number1 = 12.5
+    val number2 = 3.5
+    var result: Double
+    
+    result = number1 + number2
+    println(result)
+    result = number1 - number2
+    println(result)
+    result = number1 * number2
+    println(result)
+    result = number1 / number2
+    println(result)
+    result = number1 % number2
+    println(result)
+    
+}
