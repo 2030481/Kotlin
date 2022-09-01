441f96586f72a7b6eab4e3fda1eef40953327874
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-01 23:59:11 +0530

    Create MoreOnOperators.kt

---
+++ MoreOnOperators.kt
@@ -0,0 +1,12 @@
+/**
+ * == check if a==b (equal to)
+ * != check if a!=b (not equal to)
+ * < check if a<b (Less than)
+ * > check if a>b (Greater than)
+ * <= check if a<=b (less than or equal to)
+ * >= check if a>=b (Greater than or equal to)
+ */
+fun main() {
+
+    
+}
