a3f3a58c30bb44eebec2c389d9c2eb0a701659cb
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 22:25:36 +0530

    Create Increment_decrement.kt

---
+++ Increment_decrement.kt
@@ -0,0 +1,23 @@
+/*
+* a++ increment operator a=a+1
+* a-- decrement operator a=a-1
+*/
+fun main(){
+var number1 = 10
+    var number2 = 5
+	
+    number1++
+    println(number1)
+    
+    number2--
+    println(number2)
+    
+    /*another method to do the same*/
+    
+    ++number1
+    println(number1)
+    
+    --number2
+    println(number2)
+
+}
