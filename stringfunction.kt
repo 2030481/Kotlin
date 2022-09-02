1ed1ad44f2b688fc9cd73924d07fcc7968e122d9
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 23:49:06 +0530

    Create stringfunction.kt

---
+++ stringfunction.kt
@@ -0,0 +1,9 @@
+fun main(){
+val s = "welcome to kotlin-ravi"
+    val str = "welcome to my world"
+    /*val stringlength = s.length */
+    println(s.length)
+    println(s)
+    println(str)
+
+}
