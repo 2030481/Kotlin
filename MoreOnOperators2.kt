fe260af9c024dc29ac7f7e89de7b60f3268d09a6
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 22:07:14 +0530

    Create MoreOnOperators2.kt

---
+++ MoreOnOperators2.kt
@@ -0,0 +1,23 @@
+fun main() {
+  var number1 = 12.5
+    val number2 = 3.5
+    number1 += number2
+    println(number1)
+    
+    var number3 = 12.5
+   	number3 -= number2
+    println(number3)
+    
+     var number4 = 23.4
+   	number4 *= number2
+    println(number4)
+   
+   var number5 = 12.0 
+   	number5 /= number2
+    println(number5)
+    
+    var number6 = 23.4
+   	number6 %= number2
+    println(number6)
+    
+}
