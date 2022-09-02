b03fa6af9ef0d9cc58a78a21d710267d566ce8a5
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 23:58:00 +0530

    Update stringfunction.kt

--- stringfunction.kt
+++ stringfunction.kt
@@ -3,6 +3,7 @@ val s = "welcome to kotlin-ravi"
     val str = "welcome to my world"
     /*val stringlength = s.length */
     val stringindex = s.get(12)
+    print("The character at index 12 is:")
     println(stringindex) /*get the value or word stored at index 12 s.get(12)*/
     println(s.length)
     println(s)
