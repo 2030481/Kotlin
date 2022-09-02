fcaee5cbb95fa8604f9ad68d11623c07073892cf
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 23:55:16 +0530

    Update stringfunction.kt

--- stringfunction.kt
+++ stringfunction.kt
@@ -2,6 +2,8 @@ fun main(){
 val s = "welcome to kotlin-ravi"
     val str = "welcome to my world"
     /*val stringlength = s.length */
+    val stringindex = s.get(12)
+    println(stringindex) /*get the value or word stored at index 12 s.get(12)*/
     println(s.length)
     println(s)
     println(str)
