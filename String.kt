ade31e1223e7f1c4829f43911832f1dff4cf1f94
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-02 23:44:02 +0530

    Create String.kt

---
+++ String.kt
@@ -0,0 +1,12 @@
+/**
+ * String is very important data types
+ * String is a sequence of character it is defined by using double quotation marks.
+ * 
+ */
+fun main() {
+val s = "welcome to kotlin-ravi"
+    val str = "welcome to my world"
+    println(s)
+    println(str)
+    
+}
