d644de9898455c2e1ef18108ebf8bbf46875498b
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-08 23:59:03 +0530

    Update ArrayInKotlin.kt

--- ArrayInKotlin.kt
+++ ArrayInKotlin.kt
@@ -7,4 +7,6 @@ fun main() {
     var characterArray = arrayOf<char>('i','n','t','e','r')
     val charArray = arrayOf<char>('a','b','c','d','e')
     val i = 1
+    val otherArray = arrayOf<char>('l','m','n','o','p')
+    charArray = otherArray
 }
