43541cc22afe52d077ed6f0b8e496db11c04cd7d
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-09 23:53:10 +0530

    Update Stringinkotlin.kt

--- Stringinkotlin.kt
+++ Stringinkotlin.kt
@@ -7,5 +7,9 @@ fun main(args: Array<String>){
 	println(str[3])
 	println(str[4])
 	
+	// accessing the string elements using for loop
+	var str2 = "Geeks"
+	for(i in str2.indices){
+		print(str2[i]+" ")
 	}
 }
