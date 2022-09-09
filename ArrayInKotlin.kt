17d06b34c52fa9bb08d0da4704f2438559662c0f
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-09 23:45:30 +0530

    Update ArrayInKotlin.kt

--- ArrayInKotlin.kt
+++ ArrayInKotlin.kt
@@ -1,6 +1,9 @@
 /**
  *Array in Kotlin
  * Array is a collection of similar data types
+ *Array is one of the most fundamental data structure in practically all programming languages. 
+ *The idea behind an array is to store multiple items of the same data-type,such as an integer or string under a single variable name.
+ *Arrays are used to organize data in programming so that a related set of values can be easily sorted or searched.
  */
 fun main() {
 
