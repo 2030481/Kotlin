d661f85a43fc73ad0759dacd748c2313f38082c7
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-09 23:52:38 +0530

    Create Stringinkotlin.kt

---
+++ Stringinkotlin.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+	// accessing string elements one by one
+	var str = "Hello"
+	println(str[0])
+	println(str[1])
+	println(str[2])
+	println(str[3])
+	println(str[4])
+	
+	}
+}
