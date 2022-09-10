d94359d90e7239909e7f2c5624adaea2962c2abc
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-11 00:05:35 +0530

    Update MultipleStringInKotlin.kt

--- MultipleStringInKotlin.kt
+++ MultipleStringInKotlin.kt
@@ -1,11 +1,17 @@
 fun main(args: Array<String>) {
 // raw string - multiline string
-    var str = """My
+    /*var str = """My
         |name
         |is
         |Yash
         |Raj
         |Movies
     """.trimMargin()
-    println(str)
+    println(str) */
+    var x = "HelloThisIsRavi"
+    var y = "HelloThisIsRavi"
+    var z = "Ravi"
+    println(x===y) // true , as both are pointing to the same StringPool
+    println(x==z) //false since values are not equal
+    println(x===z) //false
 }
