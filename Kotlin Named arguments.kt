1c7171e61ae15e903aff18f6d19908a7bc1b0b13
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-18 22:22:27 +0530

    Update Kotlin Named arguments.kt

--- Kotlin Named arguments.kt
+++ Kotlin Named arguments.kt
@@ -1,4 +1,5 @@
-// default arguments in function definition name,standard and roll_no
+// default arguments in function definition
+// name,standard and roll_no
 fun student( name: String="Praveen", standard: String="IX" , roll_no: Int=11 ) {
 	println("Name of the student is: $name")
 	println("Standard of the student is: $standard")
@@ -6,10 +7,10 @@ fun student( name: String="Praveen", standard: String="IX" , roll_no: Int=11 ) {
 }
 
 fun main(args: Array<String>) {
-	val name_of_student = "Gaurav"
+	val name_of_student = "Ravi"
 	val standard_of_student = "VIII"
 	val roll_no_of_student = 25
-	// passing the argument name_of_student to name
-	// and roll_no_of_student to standard
-	student(name_of_student,roll_no_of_student)
+
+	// passing the arguments with name as defined in function
+	student(name=name_of_student,roll_no=roll_no_of_student)
 }
