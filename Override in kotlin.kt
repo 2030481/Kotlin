25bc83622b7a98e466170ce699484b0cb99c6000
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-19 23:58:21 +0530

    Update Override in kotlin.kt

--- Override in kotlin.kt
+++ Override in kotlin.kt
@@ -1,3 +1,29 @@
 /*
 *
 */
+open class ABC {
+   open val count: Int = 0
+   
+   open fun think () {
+      println("Hey!! i am thinking ")
+   }
+}
+class BCD: ABC() {
+   override val count: Int
+   
+   init{
+      count = 100
+   }
+
+   override fun think() {
+      println("I am from Child")
+   }
+   
+   fun displayCount(){
+      println("Count value is $count")
+   }
+}
+fun main(args: Array<String>) {
+   var  a = BCD()
+   a.displayCount()
+}
